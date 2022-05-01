f3c8f269011166a9c793d7325b1ee31a4e1250c9
havana723 <71638185+havana723@users.noreply.github.com>
2022-05-01 14:28:57 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Card, TextField } from "@mui/material";
+import React from "react";
+import styled from "styled-components";
+import "./App.css";
+
+const Page = styled.div`
+  width: min(100vw, 720px);
+  margin: 0 auto;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Page>
+        <div style={{ height: "120px" }} />
+        <Card variant="outlined" style={{ padding: "10px" }}>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </Card>
+      </Page>
+    </>
   );
 }
 
